1062a183fd08aeaf12f5acaf16d77e4e4f5f9fb0
Yann Lopez <l@lonewulf.net>
2016-08-22 10:23:39 +0200

    Removed the deprecated --email option when logging to the docker repository

--- packaging/quay.io/push.sh
+++ packaging/quay.io/push.sh
@@ -6,7 +6,7 @@ if [ -n $OLD_HASH ] && [ $HASH == $OLD_HASH ]; then
 elif [ $TRAVIS_PULL_REQUEST == "false" ] && [ $PORTUS_VERSION == "master" ] && [ $TRAVIS_BRANCH == "master" ]; then
   SHA=$(cat Portus/VERSION | cut -d '@' -f 2)
   docker tag quay.io/lonewulf/portus "quay.io/lonewulf/portus:${SHA}"
-  docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}" -e "." quay.io
+  docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}" quay.io
   docker push quay.io/lonewulf/portus
 else
   echo "Not pushing"
